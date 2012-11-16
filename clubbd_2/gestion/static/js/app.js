e24de21361024f914d93be02824bbe7d46a23534
OniOni <mathieu.c.sabourin@gmail.com>
2012-11-16 23:22:40 +0100

    BUC -- Essentially logging works from UI, even has some UI stuff happening.

--- clubbd_2/gestion/static/js/app.js
+++ clubbd_2/gestion/static/js/app.js
@@ -1,4 +1,8 @@
+var __user = {};
+
 var App = Ember.Application.create({
+    __self: this,
+    user: {},
     HomeView: Em.View.extend({
 	templateName: 'home'
     }),
@@ -13,19 +17,27 @@ var App = Ember.Application.create({
     }),
     ApplicationController: Ember.Controller.extend(),
     ApplicationView: Ember.View.extend({
-	templateName: 'application'
+	templateName: 'application',
     }),
     Router: Ember.Router.extend({
 	enableLogging: true,
 	root: Ember.Route.extend({
 	    shout: function () {
-		    window.alert("More information ! NOT !");
+		window.alert("More information ! NOT !");
 	    },
 	    index: Ember.Route.extend({
 		route: '/',
 		connectOutlets: function(router) {
-	    router.get('applicationController').connectOutlet('home');
+		    router.get('applicationController').connectOutlet('main', 'home');
+		    router.get('applicationController').connectOutlet('login', 'loginForm');
 		}
+	    }),
+	    logged: Em.Route.extend({
+		index: Ember.Route.extend({
+		    connectOutlets: function(router) {
+			router.get('applicationController').connectOutlet('login', 'loginTrue', {name: __user.prenom, surname: __user.nom});
+		    }
+		})
 	    }),
 	    books: Ember.Route.extend({
 		showBook: Ember.Route.transitionTo('books.book'),
@@ -36,7 +48,7 @@ var App = Ember.Application.create({
 		    },
 		    route: '/',
 		    connectOutlets: function(router) {
-			router.get('applicationController').connectOutlet('books', App.Book.all());
+			router.get('applicationController').connectOutlet('main', 'books', App.Book.all());
 		    },
 		}),
 		book: Ember.Route.extend({
@@ -45,21 +57,45 @@ var App = Ember.Application.create({
 		    enter: function () {
 			console.log("Entered book state.");
 		    },
-		    deserialize:  function(router, context){
+		    deserialize: function(router, context){
 			return App.Book.find(context.cote);
 		    },
-		    serialize:  function(router, context){
+		    serialize: function(router, context){
 			return {
 			    id: context.cote
 			}
 		    },
-		    connectOutlets:  function(router, aBook){
-			router.get('applicationController').connectOutlet('book', aBook); 
+		    connectOutlets: function(router, aBook){
+			router.get('applicationController').connectOutlet('main', 'book', aBook); 
 		    }
 		})
 	    })
 	})
-    })    
+    })
+});
+
+
+App.LoginTrueController = Em.Controller.extend(),
+App.LoginTrueView = Em.View.extend({
+    templateName: 'loginTrue'
+});
+
+App.LoginFormController = Em.Controller.extend(),
+App.LoginFormView = Em.View.extend({
+    templateName: 'loginForm',
+    submit: function () {
+	form = $('#login');
+	login(form.children('input[type=text]').val(), form.children('input[type=password]').val(), function (json) {
+	    console.log("got key");
+	    __user = json;
+	    
+	    if (json.api_key) {
+		console.log('goto root.logged.index')
+		App.router.transitionTo('root.logged.index');
+	    }
+	    // TODO: Get cookie
+	});
+    }
 });
 
 App.Book = Ember.Object.extend();

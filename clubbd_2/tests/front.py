f57ad9316071e0102dcf24de7a8483626d84ad46
OniOni <mathieu.c.sabourin@gmail.com>
2012-11-30 22:04:44 +0100

    Add selenium tests.

---
+++ clubbd_2/tests/front.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+import unittest
+import time
+
+class FrontTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.login_true = 'charlotte.fossier@insa-lyon.fr'
+        cls.pwd_true = 'aubry'
+
+        cls.login_false = 'wrong@insa-lyon.fr'
+        cls.pwd_false = 'wrong'
+
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(5)
+        
+    
+    def login(self):
+        form = self.driver.find_element_by_id('login')
+        form.find_element_by_css_selector('input[type=text]').send_keys(self.login_true)
+        form.find_element_by_css_selector('input[type=password]').send_keys(self.pwd_true)
+        form.find_element_by_tag_name('button').click()
+        
+        self.driver.find_element_by_id('loginGreeting')
+
+
+    def test_login_success(self):
+        self.driver.get('localhost:8000/gestion')
+        form = self.driver.find_element_by_id('login')
+        form.find_element_by_css_selector('input[type=text]').send_keys(self.login_true)
+        form.find_element_by_css_selector('input[type=password]').send_keys(self.pwd_true)
+        form.find_element_by_tag_name('button').click()
+        
+        greeting = self.driver.find_element_by_id('loginGreeting')
+        self.assertTrue(greeting is not None)
+
+        
+    def test_login_failure(self):
+        self.driver.get('localhost:8000/gestion')
+        form = self.driver.find_element_by_id('login')
+        form.find_element_by_css_selector('input[type=text]').send_keys(self.login_false)
+        form.find_element_by_css_selector('input[type=password]').send_keys(self.pwd_false)
+        form.find_element_by_tag_name('button').click()
+        
+        form2 = self.driver.find_element_by_id('login')
+        self.assertTrue(form2 is not None)
+
+
+
+    def test_users(self):
+        self.driver.get('localhost:8000/gestion/')
+        self.login()
+
+        self.driver.get('localhost:8000/gestion/#/users')
+        modal = self.driver.find_elements_by_id('user_modal')
+        self.assertTrue(modal is not None)
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+        
+if __name__ == '__main__':
+    unittest.main()
